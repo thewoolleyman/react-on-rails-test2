915c3a2e734684e7f971dee45075a3f93d9920a1
Chad Woolley <thewoolleyman@gmail.com>
2016-01-11 22:20:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReactOnRailsTest2::Application.config.secret_token = '105034479d74defbf351cd46c07c26e50f0d975569fd5ca4bfd830a7f76e6619cf105b0dac0ab271f78a3629260e0d0af1fd0252fde936c13cefa9d4ceacc335'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ReactOnRailsTest2::Application.config.session_store :cookie_store, key: '_react-on-rails-test2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ReactOnRailsTest2::Application.config.session_store :active_record_store
